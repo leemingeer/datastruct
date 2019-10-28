a774af1e067f72ffc5c8f105ed3a4a3f5fea5103
leemingeer <leemingeer@gmail.com>
2019-10-28 23:14:29 +0800

    add annotation

--- lca.py
+++ lca.py
@@ -12,25 +12,37 @@ class LCASolution(object):
         pass
 
     def get_lca(self, root, p, q):
+        #None: p, q both not found under root node, In this case, all subnode is
+        #traversed
+        #find p or q, because lca can't be under subroot node
+        #no need to find under root node again, just return to uppper node
         if root is None or root is p or root is q:
             return root
         
+        
         left = self.get_lca(root.left, p, q)
         right = self.get_lca(root.right, p, q)
-
+        #root node and find in its subtree recursively
+        #return the find result to the upper call
+        #if left and right are all not None then the root of this
+        # level call is lca point
         if left is not None and right is not None:
             return root
+        #only find in its right subtree, return to upper call
+        #because left not here, so lca is in upper level node
         if left is None and right is not None:
             return right
+        #the same
         if left is not None and right is None:
             return left
+        #not found in this root node, return to None to upper node
         return None
 
 
 if __name__ == '__main__':
 
     #generate a test tree by hand
-    #the n1 node is alse a Tree
+    #the n1 node is also a Tree
     n1 = Tree = Node(2) 
     n2 = n1.left = Node(3)
     n3 = n1.right = Node(4)
